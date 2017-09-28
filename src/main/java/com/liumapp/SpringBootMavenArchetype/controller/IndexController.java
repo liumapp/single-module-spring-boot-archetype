b9ce24c34cad69d13f8bcef53cee3549f6113af5
liumapp <liumapp.com@gmail.com>
2017-09-28 15:05:00 +0800

    测试demo

---
+++ src/main/java/com/liumapp/SpringBootMavenArchetype/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.liumapp.SpringBootMavenArchetype.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by liumapp on 9/28/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@Controller
+public class IndexController {
+}
